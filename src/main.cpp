b3148270cf8ff5e86927fe89ec16cba616edbad5
Nino <1005963022@qq.com>
2024-03-20 21:26:54 +0800

    添加了MPU6050的数据读取与物联网交互

---
+++ src/main.cpp
@@ -0,0 +1,116 @@
+#include <Arduino.h>
+#include "WiFi_BLE.h"
+#include "my_MPU6050.h"
+#include "GUI_Driver.h"
+
+#include "FreeRTOS.h"
+
+char serial1_Buffer[256];
+
+void IoTTaskThread(void *argument);
+TaskHandle_t IoTTaskHandle;
+
+void SensorTaskThread(void *argument);
+TaskHandle_t SensorTaskHandle;
+
+void GUITaskThread(void *argument);
+TaskHandle_t GUITaskHandle;
+
+void SerialThread(void *argument);
+TaskHandle_t SerialHandle;
+
+static void SystemData_GUI();
+
+void setup()
+{
+  Serial.begin(115200);
+
+//   Serial1.setPins(15, 16);
+//   Serial1.begin(115200);
+
+  xTaskCreatePinnedToCore(IoTTaskThread, "IoTTask", 4096, NULL, 2, &IoTTaskHandle, 1);
+
+  xTaskCreatePinnedToCore(SensorTaskThread, "SensorTask", 4096, NULL, 2, &IoTTaskHandle, 0);
+
+//   xTaskCreatePinnedToCore(GUITaskThread, "GUITask", 4096, NULL, 1, &IoTTaskHandle, 0);
+
+  xTaskCreatePinnedToCore(SerialThread, "Serial", 2048, NULL, 2, &SerialHandle, 0);
+
+  // std::stringstream urlStream;
+  // urlStream << "http://" << WiFi_Data.serverip << ":" << WiFi_Data.serverport;
+  // Serial.printf("Try to connect %s\r\n",urlStream.str().c_str());
+
+  // http.begin(urlStream.str().c_str()); //连接服务器对应域名
+  // vTaskStartScheduler();
+}
+
+void SensorTaskThread(void *argument){
+  MPU6050_setup();
+  for(;;){
+    MPU6050_getData();
+    MPU6050_SendJSONPack();
+    vTaskDelay(10);
+  }
+}
+
+void GUITaskThread(void *argument){
+  GUI_setup();
+  std::string GUI_logString = std::string(serial1_Buffer);
+  for(;;){
+    SystemData_GUI();
+    MPU6050_GUILog();
+    GUI_logPrint(GUI_logString);
+    vTaskDelay(500);
+  }
+}
+
+void IoTTaskThread(void *argument){
+  WiFi_BLE_setUp();
+  configTime(gmtOffset_sec, 0, "pool.ntp.org");
+
+  for(;;){
+    BLEHandler();
+    // WiFiHandler();
+    ProjectDataUpdate();
+    vTaskDelay(5);
+  }
+}
+
+void SerialThread(void *argument){
+  static int bufferIndex = 0;
+  for(;;){
+    if(value.length() > 0){
+      Serial.print(value.c_str());
+    }
+
+    if(Serial.available() > 0){
+      Serial.read(serial1_Buffer, 1);
+      bufferIndex++;
+
+      if(serial1_Buffer[bufferIndex] == '}'){
+        bufferIndex = 0;
+        std::string BLE_transmit = std::string(serial1_Buffer);
+        TX_Characteristics.setValue(BLE_transmit);
+        TX_Characteristics.notify();
+        memset(serial1_Buffer, 0, sizeof(serial1_Buffer));
+      }
+
+    }
+  }
+}
+
+void SystemData_GUI(){
+  GUI_sysPrint(0, 16, "                                                      ");
+  GUI_sysPrint(0, 16, "WiFi Status: %s", WiFiStatus_str[ProjectData.wifistatus]);
+  GUI_sysPrint(0, 32, "                                                      ");
+  GUI_sysPrint(0, 32, "ipv4 address: %s", WiFi.localIP().toString().c_str());
+
+  GUI_sysPrint(0, 192, "%04d-%02d-%02d %02d:%02d:%02d", timeinfo.tm_year + 1900, timeinfo.tm_mon + 1, timeinfo.tm_mday,
+                timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+  GUI_sysPrint(0, 208, "last update date: %s", __DATE__);
+}
+
+void loop()
+{
+  delay(500);
+}
